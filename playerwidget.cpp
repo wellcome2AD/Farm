85560b06e2b669730cbb46539fc098743603eb1b
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-12 14:58:28 +0300

    Update playerwidget.cpp

--- playerwidget.cpp
+++ playerwidget.cpp
@@ -4,11 +4,12 @@
 PlayerWidget::PlayerWidget(QWidget *parent) : QWidget(parent)
 {
     label = new QLabel(this);
-    QPixmap image("D:\\QtProjects\\Farm\\farm_map.png");
-    label->setFixedSize(image.size());
-    label->setPixmap(image);
+    QPixmap image("D:\\QtProjects\\Farm\\farm_map.jpg");
+    label->setPixmap(image.scaled(QSize(600, 400), Qt::AspectRatioMode::KeepAspectRatio));
+    //label->resize(image.size());
+    label->setScaledContents(true);
 
-    layout = new QBoxLayout(QBoxLayout::TopToBottom, this);
-    layout->addWidget(label);
-    setLayout(layout);
+    map_layout = new QBoxLayout(QBoxLayout::TopToBottom, this);
+    map_layout->addWidget(label);
+    setLayout(map_layout);
 }
