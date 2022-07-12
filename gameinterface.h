9bab25f20d4b50bd611d3b3b52164d6ca15b9f6a
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-12 14:52:54 +0300

    GameInterface.h updated
    добавлены новые лэйауты для разных групп объектов, а также методы, обрабатывающие нажатие на разные кнопки

--- gameinterface.h
+++ gameinterface.h
@@ -2,6 +2,9 @@
 #define GAMEINTERFACE_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include "exchangebutton.h"
 #include "game.h"
 #include "playerwidget.h"
 
@@ -10,8 +13,17 @@ class GameInterface : public QWidget
     Q_OBJECT
     Game game;
     PlayerWidget* plwidget;
-    QBoxLayout* layout;
-
+    QVBoxLayout* common_layout;
+    QBoxLayout* maps_layout;
+    QHBoxLayout* buttons_layout;
+    // ducks to goat, goats to pig, pigs to horse, horses to cows, goat to a dog, horse to a dog
+private:
+    void onExchange1ButtonClicked();
+    void onExchange2ButtonClicked();
+    void onExchange3ButtonClicked();
+    void onExchange4ButtonClicked();
+    void onExchange5ButtonClicked();
+    void onExchange6ButtonClicked();
 public:
     GameInterface(QWidget *parent = nullptr);
     ~GameInterface();
