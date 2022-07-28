40a25821e695ee91b1ec6d60350a7e9fa3036158
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-28 16:26:57 +0300

    Update animalEnum.h
    функция по переводу элементов enum  в строку

--- animalEnum.h
+++ animalEnum.h
@@ -1,3 +1,5 @@
+#include <QString>
+
 #ifndef ANIMALENUM_H
 #define ANIMALENUM_H
 
@@ -11,5 +13,27 @@ enum animalEnum
     fox,
     bear
 };
+
+inline QString animalEnumToQString(animalEnum type)
+{
+    switch (type)
+    {
+    case duck:
+        return "duck";
+    case goat:
+        return "goat";
+    case pig:
+        return "pig";
+    case horse:
+        return "horse";
+    case cow:
+        return "cow";
+    case fox:
+        return "fox";
+    case bear:
+        return "bear";
+    }
+    return "error";
+}
 
 #endif // ANIMALENUM_H
