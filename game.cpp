d70f48d417772e08582c930d05dd4c7977725f0b
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-28 16:41:22 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,10 +1,9 @@
 #include "game.h"
 
 Game::Game(int number_of_players)
-    : players(number_of_players, new Player())
 {
-    /*for(int i = 0; i < number_of_players; ++i)
-        players.append(new Player());*/
+    for(int i = 0; i < number_of_players; ++i)
+        players.append(new Player());
 }
 Game::~Game()
 {
