051947a2092c97f1cb929a2bee3268f842fb610f
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-29 17:35:55 +0300

    Update gamephasewidget.cpp

--- gamephasewidget.cpp
+++ gamephasewidget.cpp
@@ -17,35 +17,34 @@ GamePhaseWidget::GamePhaseWidget(QWidget *parent)
 {    
     QPoint parent_center(parent->x() + parent->width() / 2 - 80, parent->y() + parent->height() / 2 + 120);
     qDebug() << parent_center;
-    move(parent_center);
+    //move(parent_center);
 
     setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
     setWindowFlags(Qt::Tool | Qt::FramelessWindowHint | Qt::WindowSystemMenuHint);
 
     QVBoxLayout* layout = new QVBoxLayout(this);
+    QHBoxLayout* dice_layout = new QHBoxLayout;
+
     size_t text_size = parent->width() / 20;
     label->setMaximumHeight(50);
     label->setFont(QFont("Times", text_size, QFont::Bold));
     label->setText("GAME STARTS");
     label->setAutoFillBackground(true);
-    layout->addWidget(label, Qt::AlignHCenter);
+    label->show();
 
-    QHBoxLayout* dice_layout = new QHBoxLayout;
     dice_layout->addWidget(dice1);
     dice_layout->addWidget(dice2);
+    layout->addWidget(label, Qt::AlignHCenter);
     layout->addLayout(dice_layout, Qt::AlignHCenter);
 
     setLayout(layout);
-
-    dice1->show();
-    dice2->show();
 }
-void GamePhaseWidget::widgetFadeAnimation(const QVariant& start_value, const QVariant& end_value, int duration)
+void GamePhaseWidget::labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVariant& end_value, int duration)
 {
-    QPropertyAnimation* anim = new QPropertyAnimation(this);
-    QGraphicsOpacityEffect* grEffect = new QGraphicsOpacityEffect(this);
-    this->setGraphicsEffect(grEffect);
+    QPropertyAnimation* anim = new QPropertyAnimation(label);
+    QGraphicsOpacityEffect* grEffect = new QGraphicsOpacityEffect(label);
+    (label)->setGraphicsEffect(grEffect);
 
     anim->setTargetObject(grEffect);
     anim->setPropertyName("opacity");
@@ -53,14 +52,15 @@ void GamePhaseWidget::widgetFadeAnimation(const QVariant& start_value, const QVa
     anim->setEndValue(end_value);
     anim->setDuration(duration);
     anim->start();
-    //dice1->hide();
-    //dice2->hide();
 }
 void GamePhaseWidget::NextPhase()
 {
     current_phase = (current_phase + 1) % phase_text.size();
     label->setText(phase_text.at(current_phase));
-    qDebug() << "you are here";
+    qDebug() << current_phase << " " << phase_text.at(current_phase);
+
+    labelFadeAnimation(label, 0.0, 1.0, 500);
+    QTimer::singleShot(3500, [this]{ labelFadeAnimation(label, 1.0, 0.0, 500); });
     if(current_phase == 0)
     {
         DiceGenerator::GetDice().ThrowDice();
@@ -68,14 +68,15 @@ void GamePhaseWidget::NextPhase()
 
         QString file_path1(":/resources/dice_" + animalEnumToQString(pair_of_animals.first));
         QString file_path2(":/resources/dice_" + animalEnumToQString(pair_of_animals.second));
+        qDebug() << file_path1;
+        qDebug() << file_path2;
         QPixmap dice1_pic(file_path1);
         QPixmap dice2_pic(file_path2);
         dice1->setPixmap(dice1_pic.scaled(150, 150, Qt::AspectRatioMode::KeepAspectRatio));
         dice2->setPixmap(dice2_pic.scaled(150, 150, Qt::AspectRatioMode::KeepAspectRatio));
 
-        dice1->show();
-        dice2->show();
+        labelFadeAnimation(dice1, 0.0, 1.0, 500);
+        labelFadeAnimation(dice2, 0.0, 1.0, 500);
+        QTimer::singleShot(5000, [this]{ labelFadeAnimation(dice1, 1.0, 0.0, 500), labelFadeAnimation(dice2, 1.0, 0.0, 500); });
     }
-    widgetFadeAnimation(0.0, 1.0, 1000);
-    QTimer::singleShot(2000, this, [this]{ widgetFadeAnimation(1.0, 0.0, 1000); });
 }
