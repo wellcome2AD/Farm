2030a5e6b04fa83181679e1df0e7077075452d9b
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-29 13:05:02 +0300

    Update gamephasewidget.cpp

--- gamephasewidget.cpp
+++ gamephasewidget.cpp
@@ -1,30 +1,47 @@
 #include <QLabel>
 #include <QDebug>
 #include <QTimer>
+#include <QPropertyAnimation>
+#include <QGraphicsOpacityEffect>
 #include "gamephasewidget.h"
 
+void labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVariant& end_value, int duration);
+
 GamePhaseWidget::GamePhaseWidget(QWidget *parent)
-    : QWidget(parent)//,
-      //layout(new QVBoxLayout(this))
+    : QWidget(parent),
+      layout(new QVBoxLayout(this))
 {    
-    QPoint parent_center(parent->x() + parent->width() / 2 - 80, parent->y() + parent->height() / 2 + 100);
+    QPoint parent_center(parent->x() + parent->width() / 2 - 80, parent->y() + parent->height() / 2 + 120);
     qDebug() << parent_center;
+    move(parent_center);
 
     setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
     setWindowFlags(Qt::Tool | Qt::FramelessWindowHint | Qt::WindowSystemMenuHint);
 
     QLabel* label = new QLabel(this);
-    label->move(parent_center);
     size_t text_size = parent->width() / 20;
     label->setFont(QFont("Times", text_size, QFont::Bold));
     label->setText(phase_text.at(current_phase));
     label->setAutoFillBackground(true);
-    //label->resize(300, 80);
-    //layout->addWidget(label, Qt::AlignHCenter);
-    //setLayout(layout);
+    layout->addWidget(label, Qt::AlignHCenter);
+    setLayout(layout);
 
-    QTimer::singleShot(6000, label, &QLabel::hide);
+    labelFadeAnimation(label, 0.0, 1.0, 500);
+    QTimer::singleShot(4000, this, [label]{labelFadeAnimation(label, 1.0, 0.0, 500);});
+}
+void labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVariant& end_value, int duration)
+{
+    QPropertyAnimation* anim = new QPropertyAnimation(label);
+    QGraphicsOpacityEffect* grEffect = new QGraphicsOpacityEffect(label);
+    label->setGraphicsEffect(grEffect);
+
+    anim->setTargetObject(grEffect);
+    anim->setPropertyName("opacity");
+    anim->setStartValue(start_value);
+    anim->setEndValue(end_value);
+    anim->setDuration(duration);
+    anim->start();
 }
 void GamePhaseWidget::NextPhase()
 {
