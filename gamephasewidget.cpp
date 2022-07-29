bb7c6f5924e36e655511304a14097e1e7c4072c0
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-29 16:48:55 +0300

    dice images were added

--- gamephasewidget.cpp
+++ gamephasewidget.cpp
@@ -1,15 +1,19 @@
-#include <QLabel>
+#include <QVBoxLayout>
 #include <QDebug>
 #include <QTimer>
 #include <QPropertyAnimation>
 #include <QGraphicsOpacityEffect>
 #include "gamephasewidget.h"
+#include "dicegenerator.h"
 
 void labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVariant& end_value, int duration);
 
 GamePhaseWidget::GamePhaseWidget(QWidget *parent)
     : QWidget(parent),
-      layout(new QVBoxLayout(this))
+      label(new QLabel(this)),
+      dice1(new QLabel(this)),
+      dice2(new QLabel(this)),
+      current_phase(-1)
 {    
     QPoint parent_center(parent->x() + parent->width() / 2 - 80, parent->y() + parent->height() / 2 + 120);
     qDebug() << parent_center;
@@ -19,22 +23,29 @@ GamePhaseWidget::GamePhaseWidget(QWidget *parent)
     setAttribute(Qt::WA_TranslucentBackground);
     setWindowFlags(Qt::Tool | Qt::FramelessWindowHint | Qt::WindowSystemMenuHint);
 
-    QLabel* label = new QLabel(this);
+    QVBoxLayout* layout = new QVBoxLayout(this);
     size_t text_size = parent->width() / 20;
+    label->setMaximumHeight(50);
     label->setFont(QFont("Times", text_size, QFont::Bold));
-    label->setText(phase_text.at(current_phase));
+    label->setText("GAME STARTS");
     label->setAutoFillBackground(true);
     layout->addWidget(label, Qt::AlignHCenter);
+
+    QHBoxLayout* dice_layout = new QHBoxLayout;
+    dice_layout->addWidget(dice1);
+    dice_layout->addWidget(dice2);
+    layout->addLayout(dice_layout, Qt::AlignHCenter);
+
     setLayout(layout);
 
-    labelFadeAnimation(label, 0.0, 1.0, 500);
-    QTimer::singleShot(4000, this, [label]{labelFadeAnimation(label, 1.0, 0.0, 500);});
+    dice1->show();
+    dice2->show();
 }
-void labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVariant& end_value, int duration)
+void GamePhaseWidget::widgetFadeAnimation(const QVariant& start_value, const QVariant& end_value, int duration)
 {
-    QPropertyAnimation* anim = new QPropertyAnimation(label);
-    QGraphicsOpacityEffect* grEffect = new QGraphicsOpacityEffect(label);
-    label->setGraphicsEffect(grEffect);
+    QPropertyAnimation* anim = new QPropertyAnimation(this);
+    QGraphicsOpacityEffect* grEffect = new QGraphicsOpacityEffect(this);
+    this->setGraphicsEffect(grEffect);
 
     anim->setTargetObject(grEffect);
     anim->setPropertyName("opacity");
@@ -42,24 +53,29 @@ void labelFadeAnimation(QLabel* label, const QVariant& start_value, const QVaria
     anim->setEndValue(end_value);
     anim->setDuration(duration);
     anim->start();
+    //dice1->hide();
+    //dice2->hide();
 }
 void GamePhaseWidget::NextPhase()
 {
-    QLayoutItem* item = layout->takeAt(0);
-    if(item == nullptr)
-    {
-        qDebug() << "GamePhaseWidget: Can't get an item from layout";
-        return;
-    }
-    auto label = qobject_cast<QLabel*>(item->widget());
-    if(label == nullptr)
-    {
-        qDebug() << "GamePhaseWidget: Can't get a label from item";
-        return;
-    }
     current_phase = (current_phase + 1) % phase_text.size();
     label->setText(phase_text.at(current_phase));
-    layout->addWidget(label);
-    labelFadeAnimation(label, 0.0, 1.0, 500);
-    QTimer::singleShot(2000, this, [label]{labelFadeAnimation(label, 1.0, 0.0, 500);});
+    qDebug() << "you are here";
+    if(current_phase == 0)
+    {
+        DiceGenerator::GetDice().ThrowDice();
+        auto pair_of_animals = DiceGenerator::GetDice().GetResult();
+
+        QString file_path1(":/resources/dice_" + animalEnumToQString(pair_of_animals.first));
+        QString file_path2(":/resources/dice_" + animalEnumToQString(pair_of_animals.second));
+        QPixmap dice1_pic(file_path1);
+        QPixmap dice2_pic(file_path2);
+        dice1->setPixmap(dice1_pic.scaled(150, 150, Qt::AspectRatioMode::KeepAspectRatio));
+        dice2->setPixmap(dice2_pic.scaled(150, 150, Qt::AspectRatioMode::KeepAspectRatio));
+
+        dice1->show();
+        dice2->show();
+    }
+    widgetFadeAnimation(0.0, 1.0, 1000);
+    QTimer::singleShot(2000, this, [this]{ widgetFadeAnimation(1.0, 0.0, 1000); });
 }
--- gamephasewidget.h
+++ gamephasewidget.h
@@ -2,15 +2,18 @@
 #define GAMEPHASEWIDGET_H
 
 #include <QWidget>
-#include <QVBoxLayout>
+#include <QLabel>
 #include <QString>
 
 class GamePhaseWidget : public QWidget
 {
     Q_OBJECT
-    QVBoxLayout* layout;
+    QLabel* label;
+    QLabel* dice1,* dice2;
     const QVector<QString> phase_text{"DICE THROW", "EXCHANGE"};
-    size_t current_phase = 0;
+    size_t current_phase;
+private:
+    void widgetFadeAnimation(const QVariant& start_value, const QVariant& end_value, int duration);
 public:
     explicit GamePhaseWidget(QWidget *parent = nullptr);
     void NextPhase();
