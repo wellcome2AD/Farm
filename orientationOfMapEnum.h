4fb418f0f11fa005a5ade8d708b99aa01d59d263
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-30 14:22:10 +0300

    Update orientationOfMapEnum.h
    метод, возвращающий enum в виде строки

--- orientationOfMapEnum.h
+++ orientationOfMapEnum.h
@@ -1,3 +1,5 @@
+#include <QString>
+
 #ifndef ORIENTATIONOFMAPENUM_H
 #define ORIENTATIONOFMAPENUM_H
 
@@ -5,5 +7,17 @@ enum orientationOfMapEnum
 {
     left_side_map, right_side_map
 };
+
+inline QString orientationOfMapEnumToQString(orientationOfMapEnum type)
+{
+    switch (type)
+    {
+    case left_side_map:
+        return "left_side_map";
+    case right_side_map:
+        return "right_side_map";
+    }
+    return "error";
+}
 
 #endif // ORIENTATIONOFMAPENUM_H
