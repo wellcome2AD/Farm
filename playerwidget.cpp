29ce9eb7cd6e34ea19af22c027aa597e25d5264f
wellcome2AD <iuuyyttttt@mail.ru>
2022-07-30 14:17:25 +0300

    map with coefficents was added
    коэффициенты для поиска координат текста теперь помещены в map

--- playerwidget.cpp
+++ playerwidget.cpp
@@ -10,86 +10,80 @@ PlayerWidget::PlayerWidget(QWidget *parent, orientationOfMapEnum orientation, Pl
       orientation(orientation),
       player(player)
 {
+    coefficents.insert("left_side_map horse", QPointF(1.0/16, 1.0/16));
+    coefficents.insert("left_side_map cow",   QPointF(1.0/16, 7.0/16));
+    coefficents.insert("left_side_map pig",   QPointF(1.0/16, 13.0/16));
+    coefficents.insert("left_side_map duck",  QPointF(3.0/4, 1.0/8));
+    coefficents.insert("left_side_map goat",  QPointF(3.0/4, 11.0/16));
+    coefficents.insert("left_side_map antyfox_dog", QPointF(33.0/64, 11.0/32));
+    coefficents.insert("left_side_map antybear_dog", QPointF(66.0/128, 21.0/32));
+
+    coefficents.insert("right_side_map horse", QPointF(15.0/16, 1.0/16));
+    coefficents.insert("right_side_map cow",   QPointF(15.0/16, 7.0/16));
+    coefficents.insert("right_side_map pig",   QPointF(15.0/16, 13.0/16));
+    coefficents.insert("right_side_map duck",  QPointF(1.0/4, 1.0/8));
+    coefficents.insert("right_side_map goat",  QPointF(1.0/4, 11.0/16));
+    coefficents.insert("right_side_map antyfox_dog", QPointF(28.0/64, 11.0/32));
+    coefficents.insert("right_side_map antybear_dog", QPointF(49.0/128, 21.0/32));
+
     setFixedSize(380, 388);
     connect(player, SIGNAL(playerUpdated()), this, SLOT(onPlayerUpdate()));
-    QString map_path;
-    if(orientation == left_side_map)
-    {
-        map_path = ":/resources/left_farm_map";
-    }
-    else
-    {
-        map_path = ":/resources/right_farm_map";
-    }
+    QString map_path(":/resources/" + orientationOfMapEnumToQString(orientation));
     bool isLoaded = image.load(map_path);
     if(isLoaded == false)
     {
-        qDebug() << "image is not loaded";
+        qDebug() << "PlayerWidget: map is not loaded";
     }
     resize(400,200);
 }
 void PlayerWidget::paintEvent(QPaintEvent* event)
 {
     QWidget::paintEvent(event);
-    QPainter painter1(this);
     QPixmap copy_image = image.scaled(size(), Qt::AspectRatioMode::KeepAspectRatio);
     resize(copy_image.size());
+    QPainter painter1(this);
     QPainter painter2(&copy_image);
 
     size_t text_size = copy_image.width()/32;
     painter2.setFont(QFont("Times", text_size, QFont::Bold));
 
-    if(orientation == left_side_map)
+    QString orientaion_string = orientationOfMapEnumToQString(orientation);
+    QMap<animalEnum, int> animals= player->GetAnimals();
+    animalEnum list[] = {duck, goat, pig, horse, cow};
+    for(int i = 0; i < animals.size(); ++i)
     {
-        painter2.drawText(copy_image.width()/16, copy_image.height()/16,     QString::number(player->GetHorses()));
-        painter2.drawText(copy_image.width()/16, copy_image.height()*7/16,   QString::number(player->GetCows()));
-        painter2.drawText(copy_image.width()/16, copy_image.height()*13/16,  QString::number(player->GetPigs()));
-        painter2.drawText(copy_image.width()*3/4, copy_image.height()/8,     QString::number(player->GetDucks()));
-        painter2.drawText(copy_image.width()*3/4, copy_image.height()*11/16, QString::number(player->GetGoats()));
-    }
-    else
-    {
-        painter2.drawText(copy_image.width()*15/16, copy_image.height()/16,     QString::number(player->GetHorses()));
-        painter2.drawText(copy_image.width()*15/16, copy_image.height()*7/16,   QString::number(player->GetCows()));
-        painter2.drawText(copy_image.width()*15/16, copy_image.height()*13/16,  QString::number(player->GetPigs()));
-        painter2.drawText(copy_image.width()/4, copy_image.height()/8,     QString::number(player->GetDucks()));
-        painter2.drawText(copy_image.width()/4, copy_image.height()*11/16, QString::number(player->GetGoats()));
+        animalEnum animal = list[i];
+        QString animal_string = animalEnumToQString(animal);
+        int x = copy_image.width() * coefficents[orientaion_string + ' ' + animal_string].x();
+        int y = copy_image.height() * coefficents[orientaion_string + ' ' + animal_string].y();
+        painter2.drawText(x, y, QString::number(animals[animal]));
     }
 
     if(player->GetAntyfoxDog())
     {
         QPixmap antyfox_dog(":/resources/antyfox_dog");
         antyfox_dog = antyfox_dog.scaled(size()/10, Qt::AspectRatioMode::KeepAspectRatio);
-        if(orientation == left_side_map)
-        {
-            painter2.drawPixmap(copy_image.width()*33/64, copy_image.height()*11/32, antyfox_dog);
-        }
-        else
-        {
-            painter2.drawPixmap(copy_image.width()*28/64, copy_image.height()*11/32, antyfox_dog);
-        }
+        int x = copy_image.width() * coefficents[orientaion_string + " antyfox_dog"].x();
+        int y = copy_image.height() * coefficents[orientaion_string + " antyfox_dog"].y();
+        painter2.drawPixmap(x, y, antyfox_dog);
     }
     if(player->GetAntybearDog())
     {
         QPixmap antybear_dog(":/resources/antybear_dog");
         antybear_dog = antybear_dog.scaled(size()/10, Qt::AspectRatioMode::KeepAspectRatio);
-        if(orientation == left_side_map)
-        {
-            painter2.drawPixmap(copy_image.width()*66/128, copy_image.height()*21/32, antybear_dog);
-        }
-        else
-        {
-            painter2.drawPixmap(copy_image.width()*49/128, copy_image.height()*21/32, antybear_dog);
-        }
+        int x = copy_image.width() * coefficents[orientaion_string + " antybear_dog"].x();
+        int y = copy_image.height() * coefficents[orientaion_string + " antybear_dog"].y();
+        painter2.drawPixmap(x, y, antybear_dog);
     }
+
     painter1.drawPixmap(0, 0, copy_image);
 
     auto gameInterface = qobject_cast<GameInterface*>(parent());
     if (gameInterface && gameInterface->GetGame().GetCurrentPlayer() == player)
     {
-      constexpr int thickness = 4;
-      painter1.setPen(QPen(Qt::red, thickness));
-      painter1.drawRect(0 + thickness / 2, 0 + thickness / 2, width() - thickness, height() - thickness);
+        constexpr int thickness = 4;
+        painter1.setPen(QPen(Qt::red, thickness));
+        painter1.drawRect(0 + thickness / 2, 0 + thickness / 2, width() - thickness, height() - thickness);
     }
 }
 void PlayerWidget::onPlayerUpdate()
--- playerwidget.h
+++ playerwidget.h
@@ -11,6 +11,7 @@ class PlayerWidget : public QWidget
     Q_OBJECT
     QPixmap image;
     orientationOfMapEnum orientation;
+    QMap<QString, QPointF> coefficents;
     Player* player;
 protected:
     void paintEvent(QPaintEvent* event) override;
